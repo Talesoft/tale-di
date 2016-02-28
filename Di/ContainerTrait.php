444f16f9be2af0259020a957012c802207f0ac23
Torben Köhn <t.koehn@outlook.com>
2016-02-28 16:35:29 +0100

    Minor update

--- Di/ContainerTrait.php
+++ Di/ContainerTrait.php
@@ -2,15 +2,44 @@
 
 namespace Tale\Di;
 
+/**
+ * Class ContainerTrait
+ * @package Tale\Di
+ */
 trait ContainerTrait
 {
 
     /**
+     * The dependencies registered on this container.
+     *
      * @var Dependency[]
      */
     private $_dependencies = [];
+
+    /**
+     * A cache for faster resolving of dependencies.
+     *
+     * @var Dependency[]
+     */
     private $_dependencyFindCache = [];
 
+    /**
+     * Returns all dependencies registered on this container.
+     *
+     * @return Dependency[]
+     */
+    public function getDependencies()
+    {
+        return $this->_dependencies;
+    }
+
+    /**
+     * Finds a dependency based on its class-name.
+     *
+     * @param $className
+     *
+     * @return Dependency|null
+     */
     public function findDependency($className)
     {
 
@@ -34,12 +63,23 @@ trait ContainerTrait
         return $result;
     }
 
+    /**
+     * @param $className
+     *
+     * @return bool
+     */
     public function has($className)
     {
 
         return $this->findDependency($className) !== null;
     }
 
+    /**
+     * @param $className
+     *
+     * @return null|object
+     * @throws \Exception
+     */
     public function get($className)
     {
 
@@ -54,6 +94,12 @@ trait ContainerTrait
         return $dep->getInstance();
     }
 
+    /**
+     * @param      $className
+     * @param bool $persistent
+     *
+     * @return $this
+     */
     public function register($className, $persistent = true)
     {
 
@@ -61,6 +107,12 @@ trait ContainerTrait
             throw new \RuntimeException(
                 "Failed to register dependency: ".get_class($this)." uses ".
                 ContainerTrait::class.", but doesnt implement ".ContainerInterface::class
+            );
+
+        if ($this->has($className))
+            throw new \RuntimeException(
+                "Failed to register dependency $className: This dependency ".
+                "is already registered. Use a sub-class to avoid ambiguity"
             );
 
         /** @var ContainerInterface|ContainerTrait $this */
@@ -74,6 +126,9 @@ trait ContainerTrait
         return $this;
     }
 
+    /**
+     * @return $this
+     */
     public function registerContainer()
     {
 
